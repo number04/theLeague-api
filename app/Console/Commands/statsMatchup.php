dad359536bd8d41a33e33221ff4f7f600476c81b
number04 <schn.matthew@gmail.com>
2019-02-19 19:37:23 +1300

    stats

--- app/Console/Commands/statsMatchup.php
+++ app/Console/Commands/statsMatchup.php
@@ -38,6 +38,6 @@ class statsMatchup extends Command
      */
     public function handle(CommandRepository $command)
     {
-        $command->statsMatchup('20', '2019-02-11', '2019-02-18');
+        $command->statsMatchup('21', '2019-02-18', '2019-02-25');
     }
 }
